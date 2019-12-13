3a6bd0ca6aeb5671b3030d277823aa5f64e92f89
vkku <kumar.vivekten@gmail.com>
2019-12-13 23:17:32 +0530

    Added CORS

--- src/main/java/com/hotelsingapore/controller/SingaporeHotelController.java
+++ src/main/java/com/hotelsingapore/controller/SingaporeHotelController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +16,7 @@ import com.hotelsingapore.repository.HotelRepository;
 import com.hotelsingapore.service.SingaporeHotelService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class SingaporeHotelController {
 	
 	@Autowired
